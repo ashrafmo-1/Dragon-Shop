f88aed614d2930396479b39022b9dff2180eaa1c
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-28 16:02:05 +0200

    folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
       <h1>start dradon ecommerce</h1>
-    </main>
   );
 }
