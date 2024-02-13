f5623c3b6006aac0d5ece2bd86183e239a4acc65
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-13 10:02:55 +0200

    end show users

--- src/app/dashboard/show-all-users/page.jsx
+++ src/app/dashboard/show-all-users/page.jsx
@@ -5,17 +5,25 @@ import "./globals.css";
 const Show_all_users = () => {
     const [users, setusers] = useState([])
     let api = "http://127.0.0.1:8000/api/user/show";
-
     useEffect(() => {
-        fetch(api).then((response) => response.json()).then((data) => setusers(data))
+        fetch(api)
+        .then((response) => response.json())
+        .then((data) => setusers(data))
+        .catch((error) => new Error(error));
     }, [])
 
-    // const response = await fetch(api, {
-    //     next: {
-    //         revalidate: 60,
-    //     }
-    // });
-    // let users = await response.json();
+    let all_users = users.map((user, index) => {
+        return (
+            <tr className='text-center' key={index}>
+                <td> {user.id} </td>
+                <td> {user.name} </td>
+                <td> {user.email} </td>
+                <td className='d-flex justify-content-center gap-3'>
+                    <button className='btn btn-outline-success fs-5 py-1 px-2'>Edit</button>
+                </td>
+            </tr>
+        )})
+
     return (
         <div className="user_management">
             <h1 className='my-4 text-center' style={{textTransform: 'uppercase', color: 'teal'}}>users management 👨‍💻👩‍💻</h1>
@@ -28,20 +36,8 @@ const Show_all_users = () => {
                         <th scope="col" className='py-2 fs-4' style={{textTransform: 'capitalize'}}>action</th>
                         </tr>
                     </thead>
-                    <tbody className="table-group-divider"> 
-                        {   users.map((user, index) => {
-                            return (
-                                <tr className='text-center' key={index}>
-                                    <td> {user.id} </td>
-                                    <td> {user.name} </td>
-                                    <td> {user.email} </td>
-                                    <td className='d-flex justify-content-center gap-3'>
-                                        <button className='btn btn-outline-success fs-5 py-1 px-2'>Edit</button>
-                                    </td>
-                                </tr>
-                            )
-                            })
-                        }
+                    <tbody className="table-group-divider">
+                        {all_users}
                     </tbody>
                 </table>
         </div>
