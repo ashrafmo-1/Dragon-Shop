81a83537c475f1514d44967531f39ee854151378
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-20 09:18:17 +0200

    edit product card style

--- src/app/fetchproduct/boxes_products.jsx
+++ src/app/fetchproduct/boxes_products.jsx
@@ -13,27 +13,24 @@ const Boxes_products = async () => {
   const products_boxes = products.map((product) => {
     return (
       <div className='product_box'>
-        <p className='product-description mb-4'> {product.category} </p>
-        <div className='image-control-product'>
-          <img src={product.image} width={75} height={75} />
-          <div className='controls d-flex align-items-center gap-2 flex-column'>
-            <Image className='img-control' src={cart} width={30} height={30} alt='' />
-            <Image className='img-control' src={showProduct} width={22} height={22} alt='' />
-          </div>
-        </div>
-        {/* {product.title.length > 10 ? `${product.title.slice(0, 22)}..` : product.title} */}
+        <p className='product-category mb-4'> {product.category} </p>
+          <img className='product_image' src={product.image} width={75} height={75} />
         <div className='content_box'>
           <div className='info'>
             <h3 className='product-title mt-4'> {product.title.length > 12 ? `${product.title.slice(0, 22)}..` : product.title}  </h3>
+            <p className='rate'> Rate: {product.rating.rate} </p>
           </div>
-          <p className='rate'> {product.rating.rate} </p>
+          <div className='product_controls  d-flex gap-1'>
+            <Image className='add_to_cart' src={cart} width={36} height={36} alt='add to cart' />
+            <Image className='show_all_information' src={showProduct} width={36} height={36} alt='add to cart' />
+          </div>
         </div>
       </div>
     )
   })
 
     return (
-        <div className='get_products mt-4 bg-body-secondary' >
+        <div className='get_products mt-4 bg-body-secondary'>
             <div className='products-section-title mb-5 text-center'>
                 <h2 className='title'>The most important products</h2>
             </div>
