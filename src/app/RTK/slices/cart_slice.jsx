922133041771312133015125fe075c08613f182f
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-03-14 04:27:54 +0200

    controls products cart

--- src/app/RTK/slices/cart_slice.jsx
+++ src/app/RTK/slices/cart_slice.jsx
@@ -7,10 +7,40 @@ export const Product_control = createSlice({
 
   reducers: {
     add: (state, action) => {
-      state.push(action.payload);
+      // state.push(action.payload); error add product coping
+      const findProduct = state.find((product) => {
+        product.id === action.payload.id;
+      });
+      if(findProduct) {
+        findProduct.quantity += 1;
+      } else {
+        const cloneProduct = {...action.payload, quantity: 1}
+        state.push(cloneProduct);
+      }
+    },
+    increment: (state, action) => {
+      const findProduct = state.find((product) => {
+        product.id === action.payload.id;
+      });
+      findProduct.quantity += 1;
+    },
+    decrement: (state, action) => {
+      const findProduct = state.find((product) => {
+        product.id === action.payload.id;
+      })
+      findProduct.quantity -= 1;
+      if(findProduct.quantity === 0) {
+        findProduct.quantity = 1;
+      }
+    },
+    remove: (state, action) => {
+      return state.filter((product) => product.id !== action.payload.id);
+    },
+    remove_all: () => {
+      return [];
     },
   },
 });
 
-export const { add } = Product_control.actions;
+export const { add, increment, decrement, remove, remove_all } = Product_control.actions;
 export default Product_control.reducer;
--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,18 +1,36 @@
 "use client";
-import React, { useState } from "react";
 import "./globals.scss";
-import Image from "next/image";
+import React, { useState } from "react";
 import Nav from "../components/navgationbar/Nav";
-import x from "./list.png";
 import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, remove, remove_all } from "../RTK/slices/cart_slice";
 
 const ProductCart = () => {
-  // const [show, setShow] = useState(false);
-  // const dispatch = useDispatch();
-
-  // get product from cart
+  const dispatch = useDispatch();
+  // get product from reduxToolKit
   const products = useSelector((state) => state.cart); // Fixed error reading 'map' by correctly returning state.cart;
-  console.log(products);
+
+  const totalPrice = products.reduce((acc, products) => {
+    acc += products.price;
+    return acc * products.quantity // error not a number on run
+  }, 0);
+
+  /* product cart from reduxToolKit */
+  const product = products.map((product, index) => (
+    <div className="product p-2 d-flex align-items-center justify-content-around mb-4" key={index}>
+      <img className="product_image" src={product.image} alt="Image" width={60} height={60} />
+      <div className="product_info">
+        <p className="catigory_title"> {product.category} </p>
+        <h2 className="product_title"> {product.title.slice(0, 12)} </h2>
+      </div>
+      <div className="product_control d-flex gap-2 align-items-center">
+        <span className="decrement" onClick={() => dispatch(decrement(product))}>-</span>
+        <span className="product_count d-flex align-items-center justify-content-center">{product.quantity }</span>
+        <span className="increment" onClick={() => dispatch(increment(product))}>+</span>
+      </div>
+      <button className="reamove_product btn" onClick={() => dispatch(remove(product))}>Delete</button>
+    </div>
+  ))
 
   return (
     <div className="bg-body-secondary" style={{ height: "100vh" }}>
@@ -21,31 +39,23 @@ const ProductCart = () => {
         <div className="all_products w-75 mb-4">
           <h2 className="cart_title mb-3">Shopping Cart</h2>
           <div className="all_product mt-5">
-            {products.length > 0 ?
-              products.map((product) => (
-                <div className="product p-2 d-flex align-items-center justify-content-around mb-4" key={product.id}>
-                  <img className="product_image" src={product.image} alt="Image" width={60} height={60} />
-                  <div className="product_info">
-                    <p className="catigory_title"> {product.category} </p>
-                    <h2 className="product_title"> {product.title.slice(0, 12)} </h2>
-                  </div>
-                  <div className="product_control d-flex gap-2 align-items-center">
-                    <span className="decrement">-</span>
-                    <span className="product_count d-flex align-items-center justify-content-center">
-                      1
-                    </span>
-                    <span className="increment">+</span>
-                  </div>
-                  <button className="reamove_product btn"> Delete </button>
-                </div>
-              )) : <p>not have any products</p>
-            }
+            {products.length > 0 ? (
+              <>
+                { product }
+                <button className="removeAll" onClick={() => dispatch(remove_all())}>delete all</button>
+              </>
+            ) : (
+              <p className="cart_impty">" not have any products "</p>
+              )}
           </div>
         </div>
-        <div className="total w-25 bg-secondary"></div>
+        <div className="total w-25 bg-secondary">
+          <p>totla ${totalPrice}</p>
+        </div>
       </div>
     </div>
   );
 };
-
+{/* <button></button> */}
+// 
 export default ProductCart;
